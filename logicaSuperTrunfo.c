e80c0be956c25de9f6f8f4c734ca898a960cb8da
Navar Kartalian <alpha.hunter88@gmail.com>
2025-02-10 00:06:51 +0000

    feat(01): Initiate card values

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
@@ -38,6 +18,53 @@ int main() {
 
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+
+    struct CityCard {
+    char name[50];
+    char code[4];
+    int population;
+    float area;
+    int pib;
+    int numOfPoints;
+    };
+
+    struct CityCard userCity;
+    struct CityCard computerCity = {
+        "New York",
+        "A01",
+        8258000,
+        783.8,
+        1286000000,
+        1280,
+    };
+    
+    printf("Insira o nome: \n");
+    scanf("%49s", userCity.name);
+
+    printf("Insira o código: \n");
+    scanf("%3s", userCity.code);
+
+    printf("Insira a população: \n");
+    scanf("%d", &userCity.population);
+
+    printf("Insira a área: \n");
+    scanf("%f", &userCity.area);
+
+    printf("Insira o pib: \n");
+    scanf("%d", &userCity.pib);
+
+    printf("Insira o número de pontos turísticos: \n");
+    scanf("%d", &userCity.numOfPoints);
+
+    printf("----\n");
+
+    printf("Card A: \n Nome: %s, \n Código: %s, \n População: %d, \n Área: %fkm², \n PIB: %d, \n Número de pontos turísticos: %d, \n",
+    userCity.name, userCity.code, userCity.population, userCity.area, userCity.pib, userCity.numOfPoints);
+
+    printf("----\n");
+
+    printf("Card B: \n Nome: %s, \n Código: %s, \n População: %d, \n Área: %fkm², \n PIB: %d, \n Número de pontos turísticos: %d, \n",
+    computerCity.name, computerCity.code, computerCity.population, computerCity.area, computerCity.pib, computerCity.numOfPoints);
 
     return 0;
 }
